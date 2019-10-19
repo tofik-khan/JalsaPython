d71bf761aac505c4db4aad496a9b298ce79a467d
tofik-khan <55766480+tofik-khan@users.noreply.github.com>
2019-10-19 16:13:12 -0700

    Created space for each person to write code

---
+++ Misc/StatCalculation.py
@@ -0,0 +1,12 @@
+##########TOFIK############
+print("hello world")
+print("this code is written by Tofik")
+###########################
+
+########## [NAME 1] #########
+print("this code is written by Name 1")
+###########################
+
+######### [NAME 2] ##########
+print("this code is written by Name 2")
+###########################
