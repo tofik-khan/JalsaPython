f44da1bcf353b2643fda43bfb49e31fffa7fd3aa
tofik-khan <55766480+tofik-khan@users.noreply.github.com>
2019-10-25 08:39:06 -0700

    Added percentage function
    The percentage function calculates the percentage of a value provided in arguments with respect to a total.

--- Misc/StatCalculation.py
+++ Misc/StatCalculation.py
@@ -1,12 +1,9 @@
-##########TOFIK############
-print("hello world")
-print("this code is written by Tofik")
-###########################
 
-########## [NAME 1] #########
-print("this code is written by Name 1")
-###########################
 
-######### [NAME 2] ##########
-print("this code is written by Name 2")
-###########################
+#Calculating Percentage with a value and total given
+def percentage(value, total):
+	#create a variable that calculates percentage of value/total
+
+	#return the variable
+	return #[Replace this with return variable name]
+
