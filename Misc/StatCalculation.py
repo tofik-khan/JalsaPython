f3f1ae10abab1485fb728d28946037ce4ebba61e
ambajwa99 <56981140+ambajwa99@users.noreply.github.com>
2019-10-27 23:21:01 +0000

    Update StatCalculation.py

--- Misc/StatCalculation.py
+++ Misc/StatCalculation.py
@@ -3,7 +3,7 @@
 #Calculating Percentage with a value and total given
 def percentage(value, total):
 	#create a variable that calculates percentage of value/total
-
+	calc = value/total*100
 	#return the variable
-	return #[Replace this with return variable name]
-
+	return calc
+	
